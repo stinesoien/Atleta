08b14adc1d930d813d151cb80420aa40919f0ab6
kristinweiseth <kristin.weiseth@gmail.com>
2017-09-28 13:59:12 +0200

    Bruker.java

---
+++ src/main/java/Bruker.java
@@ -0,0 +1,21 @@
+/**
+ * Created by kristinweiseth on 28.09.2017.
+ */
+public class Bruker {
+    public String epost;
+    public String passord;
+    public String fnavn;
+    public String enavn;
+    public String adresse;
+    public int mobilnr;
+
+    public Bruker(String epost, String passord, String fnavn, String enavn, String adresse, int mobilnr){
+        this.epost=epost;
+        this.passord=passord;
+        this.fnavn=fnavn;
+        this.enavn=enavn;
+        this.adresse=adresse;
+        this.mobilnr=mobilnr;
+
+    }
+}
